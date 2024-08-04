74b3ef25da72367f693df1f257644cb60b54d263
Ivan Gankevich <igankevich@capybaramail.xyz>
2024-08-04 10:53:01 +0200

    License.

--- scripts/openwrt.sh
+++ scripts/openwrt.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# build docker container from the latest openwrt rootfs
+# Build Docker image from the specified OpenWRT rootfs.
 
 cleanup() {
     rm -rf "$workdir"
@@ -12,13 +12,12 @@ version=23.05.4
 url=https://downloads.openwrt.org/releases/$version/targets/x86/64/openwrt-$version-x86-64-rootfs.tar.gz
 image=koti/openwrt-rootfs:"$version"
 workdir="$(mktemp -d)"
-curl --silent "$url" | tar -xzf- -C "$workdir"
+curl --silent --fail --location "$url" | tar -xzf- -C "$workdir"
 mkdir -p "$workdir"/var/lock
 cat >"$workdir"/Dockerfile <<'EOF'
 FROM scratch
 COPY . /
-RUN opkg update && opkg install iptables-zz-legacy bind-dig tcpdump
+RUN opkg update && opkg install iptables iptables-zz-legacy bind-dig tcpdump
 CMD ["/bin/sh"]
 EOF
 docker build --tag "$image" "$workdir"
-#docker push "$image"
